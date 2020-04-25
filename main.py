25070191331f30449b1411bd8955b1d8ef8be6b0
Adrian Zech <adrian@zech.co>
2020-04-25 18:43:35 +0200

    Append file from file dialog

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-import gi, os
+from gi.repository import Gtk
+import gi
+import os
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
 
 builder = Gtk.Builder()
 builder.add_from_file("ui/main.glade")
 
-liststore = builder.get_object("liststore")
 
-def additem(button):
-    path = builder.get_object("entry_path").get_text()
+def add_item(path):
     size = os.path.getsize(path)
+    liststore = builder.get_object("liststore")
     liststore.append([path, round(size / 1024 / 1024, 2)])
 
+
+def on_button_add_clicked(self):
+    # Get path from text field
+    path = builder.get_object("entry_path").get_text()
+    add_item(path)
+
+
 def on_selection_changed(selection):
+    # Get treeview selection
     (model, iter) = selection.get_selected()
 
     if iter is not None:
@@ -22,29 +30,28 @@ def on_selection_changed(selection):
         print("")
     return True
 
-def open_file_dialog(self):
 
+def open_file_dialog(self):
     # Set dialog
     file_dialog = Gtk.FileChooserDialog(
-                title = "Select the File", parent = None, action = Gtk.FileChooserAction.OPEN)
-    
+        title="Select the File", parent=None, action=Gtk.FileChooserAction.OPEN)
+
     # Set buttons for dialog
-    file_dialog.add_buttons ("Cancel", Gtk.ResponseType.CANCEL,"Open", Gtk.ResponseType.OK) 
+    file_dialog.add_buttons(
+        "Cancel", Gtk.ResponseType.CANCEL, "Open", Gtk.ResponseType.OK)
+
     response = file_dialog.run()
 
     # Button responses
     if response == Gtk.ResponseType.OK:
-        print("Open Button")
-        print("File: "+ file_dialog.get_filename())
-        print("Size: "+ str(os.path.getsize(file_dialog.get_filename())))
-    elif response == Gtk.ResponseType.CANCEL:
-        print("Cancel")
+        add_item(file_dialog.get_filename())
 
-    #Close dialog after finishing
+    # Close dialog after finishing
     file_dialog.destroy()
 
+
 handlers = {
-    "on_button_add_clicked": additem,
+    "on_button_add_clicked": on_button_add_clicked,
     "on_selection_changed": on_selection_changed,
     "on_botton_browse_clicked": open_file_dialog
 }
