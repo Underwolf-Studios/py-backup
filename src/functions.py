7804082b48ad7b6a0dda5f4120b83ebfda05774c
Adrian Zech <adrian@zech.co>
2020-04-29 14:15:57 +0200

    Added ability to add date and version to filename

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 import utils
+import datetime
 
 
 def add_item(liststore, path):
@@ -19,15 +20,26 @@ def remove_item(liststore, path):
     utils.message("info", "Not implemented yet")
 
 
-def backup(liststore, filename):
+def backup(liststore, filename, check_date, check_version, builder):
     files = {}
+    name = filename
+    date = "{0:%Y%m%d_%H%M}".format(datetime.datetime.now())
+
+    spin_version = builder.get_object("spin_version")
+
+    if check_date == True and check_version == False:
+        name = f"{filename}_{date}"
+    elif check_date == True == False and check_version == True:
+        name = f"{filename}_v{str(spin_version.get_value_as_int())}"
+    elif check_date == True and check_version == True:
+        name = f"{filename}_{date}_v{str(spin_version.get_value_as_int())}"
 
     # Create zipfile
-    with zipfile.ZipFile(filename + ".zip", 'w') as zf:
+    with zipfile.ZipFile(name + ".zip", 'w') as zf:
         for i in range(0, len(liststore)):
             # Add all paths and sizes to the files dict
             files[liststore[i][0]] = liststore[i][1]
             zf.write(liststore[i][0])
 
     # Calculate total size and show dialog
-    utils.message("info", filename + ".zip created. Total filesize: " + str(round(sum(files.values()), 2)) + "MB")
+    utils.message("info", name + ".zip created. Total filesize: " + str(round(sum(files.values()), 2)) + "MB")
--- src/main.py
+++ src/main.py
@@ -7,9 +7,13 @@ import utils
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-
+# Builder
 builder = Gtk.Builder()
 builder.add_from_file("src/ui/main.glade")
+
+# Windows
+window_main = builder.get_object("window_main")
+window_main.connect('delete-event', Gtk.main_quit)
 
 window_backup = builder.get_object("window_backup")
 window_backup.connect('delete-event', window_backup.close)
@@ -17,7 +21,12 @@ window_backup.connect('delete-event', window_backup.close)
 window_create = builder.get_object("window_create")
 window_create.connect('delete-event', window_create.close)
 
+# Objects
 liststore = builder.get_object("liststore_files")
+
+# Variables
+check_date = False
+check_version = False
 
 
 class Handlers:
@@ -63,19 +72,33 @@ class Handlers:
     def on_button_create_clicked(self, button):
         try:
             filename = builder.get_object("entry_filename").get_text()
-            functions.backup(liststore, filename)
-        except:
+            functions.backup(liststore, filename, check_date, check_version, builder)
+        except IOError:
             utils.message("error", "Sorry, something went wrong.")
+            print(IOError)
 
         window_create.close()
         window_backup.close()
 
+    def on_check_date_toggled(self, button):
+        global check_date
+
+        if button.get_active():
+            check_date = True
+        else:
+            check_date = False
+
+    def on_check_version_toggled(self, button):
+        global check_version
+
+        if button.get_active():
+            check_version = True
+        else:
+            check_version = False
+
 
 builder.connect_signals(Handlers())
 
-
-window_main = builder.get_object("window_main")
-window_main.connect('delete-event', Gtk.main_quit)
 window_main.show_all()
 
 Gtk.main()
